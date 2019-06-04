7afd6286b135ec41710d5911970d49a3226ca034
Mirosalva <sunwenjiayou@163.com>
2019-06-04 16:58:00 +0800

    lc13

---
+++ src/wen/leetcode/LeetCode13.java
@@ -0,0 +1,28 @@
+package wen.leetcode;
+
+
+public class LeetCode13 {
+
+    public static void main(String[] args){
+
+        System.out.println("x is Palindrome number ? "  + wen.leetcode.LeetCode9.Solution.isPalindrome(321));
+
+    }
+
+
+    /**
+     * 9. Palindrome Number
+     */
+    public static class Solution {
+
+        public int romanToInt(String s) {
+
+            return 0;
+        }
+
+        }
+
+    }
+
+
+
